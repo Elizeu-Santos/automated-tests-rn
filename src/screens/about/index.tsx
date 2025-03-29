e90e1f1b6ce0f465656f11a96cad268160813d40
Matheus Fraga <devfraga@gmail.com>
2025-03-29 13:16:23 -0400

    Projeto inicial (youtube testes automatizados)

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export function AboutScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sobre</Text>
+      <Text>Pagina Sobre</Text>
     </View>
   );
 }
